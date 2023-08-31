a95ae9aa1092953a70262b1cc691d26a692a5ac4
Michael-Mai <kienchimai@gmail.com>
2023-08-31 17:22:49 +0700

    + fixed chet not working

--- game_manager.py
+++ game_manager.py
@@ -8,12 +8,20 @@ import random
 
 
 
+#board = [
+#    [-1, -1, -1, -1, -1],
+#    [-1,  0,  0,  0, -1],
+#    [ 1,  0,  0,  0, -1],
+#    [ 1,  0,  0,  0,  1],
+#    [ 1,  1,  1,  1,  1]
+#]
+
 board = [
-    [-1, -1, -1, -1, -1],
-    [-1,  0,  0,  0, -1],
-    [ 1,  0,  0,  0, -1],
-    [ 1,  0,  0,  0,  1],
-    [ 1,  1,  1,  1,  1]
+    [1, 0, 0, 1, 0],
+    [0,  0,  0,  1, 0],
+    [0,  -1,  0,  -1, 0],
+    [0,  0,  0,  1,  0],
+    [0,  0,  -1,  0,  0]
 ]
 
 diag_pos = [(1,1), (1, 3), (3,1), (3,3)]
@@ -45,23 +53,23 @@ def toggle_turn():
 # Is the main running function in game_manager
 def run_game(board):
     engine_side = CGBot(assign_side(), game_state["board"])
-    user_side = UserBot(-engine_side.my_piece, game_state["board"])
     winner = None
 
     while winner is None:
         if engine_side.my_piece == game_state["current_turn"]:
             engine_side.activate()
             game_state["board"] = board
-        if user_side.my_piece == game_state["current_turn"]:
-            board = user_side.activate()
-            game_state["board"] = board
+        else:
+            pass
 
-        chet_remove = chet(game_state["current_turn"], -game_state["current_turn"], game_state["board"])
         ganh_remove = ganh(game_state["current_turn"], -game_state["current_turn"], game_state["board"])
+        chet_remove = chet(game_state["current_turn"], -game_state["current_turn"], game_state["board"])
 
         update_board(game_state["board"], ganh_remove, chet_remove)
 
+        print("AFTER: \n----------")
         display()
+        print("----------")
         toggle_turn()
         winner = game_over()
 
@@ -76,12 +84,14 @@ def ganh(piece, opp_piece, board):
     valid_remove = []
     opp_remove = []
     print(piece)
-    print("ganh:\n")
+
+    print("BEFORE \n----------")
     display()
-    print("-----------")
+    print("----------")
+
     game_state["board"] = board
-    chet_pair = [[(1,0), (-1, 0)], [(0,1), (0, -1)]]
-    chet_pair2 = [[(1,0), (-1, 0)], [(0,1), (0, -1)], [(1,1), (-1,-1)], [(-1,1), (1,-1)]]
+    ganh_pair = [[(1,0), (-1, 0)], [(0,1), (0, -1)]]
+    ganh_pair2 = [[(1,0), (-1, 0)], [(0,1), (0, -1)], [(1,1), (-1,-1)], [(-1,1), (1,-1)]]
 
     pieces_pos = get_position(piece)
 
@@ -90,26 +100,27 @@ def ganh(piece, opp_piece, board):
         print(position)
         if (position["y"], position["x"]) in diag_pos:
             # Loops through each pair and conclude the opponent piece
-            for pair in chet_pair2:
+            for pair in ganh_pair2:
                 for y, x in pair:
                     new_posx = position["x"] + x
                     new_posy = position["y"] + y
                     if 0 <= new_posy < len(board) and 0 <= new_posx < len(board[0]):
                         if board[new_posy][new_posx] == opp_piece:
                             opp_remove.append((new_posy, new_posx))
-                            print(opp_remove)
+                            print("Recieved ganh2:", opp_remove)
 
                 # Check if we have a pair
                 if len(opp_remove) < 2:
                     opp_remove = []
                     pass
                 else:
+                    print("VALID GANH2")
                     valid_remove.extend(opp_remove)
                     opp_remove = []
 
         else:
             # Loops through each pair and conclude the opponent piece
-            for pair in chet_pair:
+            for pair in ganh_pair:
                 for y, x in pair:
                     new_posx = position["x"] + x
                     new_posy = position["y"] + y
@@ -117,6 +128,7 @@ def ganh(piece, opp_piece, board):
                         if 0 <= new_posy < len(board) and 0 <= new_posx < len(board[0]):
                             if board[new_posy][new_posx] == opp_piece:
                                 opp_remove.append((new_posy, new_posx))
+                                print("Recieved ganh:", opp_remove)
                     except IndexError:
                         pass
 
@@ -124,12 +136,12 @@ def ganh(piece, opp_piece, board):
                     opp_remove = []
                     pass
                 else:
-                    print("VALID!")
+                    print("VALID GANH")
                     valid_remove.extend(opp_remove)
                     opp_remove = []        
 
 
-    print(valid_remove)
+    print("GANH VALID:", valid_remove)
     return valid_remove
 
 
@@ -154,6 +166,7 @@ def chet(piece, opp_piece, board):
                         if 0 <= new_posy < len(board) and 0 <= new_posx < len(board[0]):
                             if board[new_posy][new_posx] == piece:
                                 ally_chet.append((new_posy, new_posx))
+                                print("Recieved chet2:", ally_chet)
 
                     except IndexError:
                         pass
@@ -161,6 +174,7 @@ def chet(piece, opp_piece, board):
                     ally_chet = []
                     pass
                 else:
+                    print("CHET VALID2")
                     valid_remove.append((position["y"], position["x"]))
                     ally_chet = []
 
@@ -174,16 +188,20 @@ def chet(piece, opp_piece, board):
                         if 0 <= new_posy < len(board) and 0 <= new_posx < len(board[0]):
                             if board[new_posy][new_posx] == piece:
                                 ally_chet.append((new_posy, new_posx))
+                                print("Recieved chet:", ally_chet)
 
                     except IndexError:
                         pass
 
-                    if len(ally_chet) < 2:
-                        ally_chet = []
-                        pass
-                    else:
-                        valid_remove.append((position["y"], position["x"]))
-                        ally_chet = []
+                if len(ally_chet) < 2:
+                    ally_chet = []
+                    pass
+                else:
+                    print("CHET VALID")
+                    valid_remove.append((position["y"], position["x"]))
+                    ally_chet = []
+
+    print("CHET VALID:", valid_remove)
     return valid_remove
 
 
@@ -196,7 +214,7 @@ def update_board(board, ganh_remove=None, chet_remove=None):
     if chet_remove:
         execute.extend(chet_remove)
 
-    print(execute, "|")
+    print("EXECUTION:", execute)
     for row, column in execute:
         board[row][column] = 0
 
